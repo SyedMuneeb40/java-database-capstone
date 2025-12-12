6c77bbedf42134fda7a171bb83867b81477be358
Syed Muneeb <syedmuneebahmed40@gmail.com>
2025-12-12 21:04:19 +0500

    Initial project setup in IntelliJ

---
+++ src/main/java/com/smartclinicmanagement/SmartClinicManagementApplication.java
@@ -0,0 +1,13 @@
+package com.smartclinicmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartClinicManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartClinicManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smartclinicmanagement/SmartClinicManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartclinicmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartClinicManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
